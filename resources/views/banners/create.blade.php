bf74763ea1e56ebaeff3469cac299e688565a3f6
Dina Reda <engdondon92@gmail.com>
2023-03-08 08:30:18 +0300

    Banner UI structure

--- resources/views/banners/create.blade.php
+++ resources/views/banners/create.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.admin-master')
 
 @section('content')
-    <title>إضافة مقال</title>
+    <title>إضافة بانر</title>
     <div class="container">
-        <form action="{{ route('banner.store') }}" method="post" enctype="multipart/form-data" autocomplete="off">
-            {{ csrf_field() }}
-                <div class='row'>
+        <div class="row">
+            <form action="{{ route('banner.store') }}" method="post" enctype="multipart/form-data" autocomplete="off">
+                {{ csrf_field() }}
+                <div class='col-12 text-end'>
                     <label for="logo">البانر</label>
                     <input type="file" name="logo" class="dropify" accept=".pdf,.jpg, .png, image/jpeg, image/png"
                         data-height="70" />
                     <p class="text-danger">* صيغة المرفق pdf, jpeg ,.jpg , png </p>
                 </div><br>
-            </div>
-            <button type="submit" class="btn btn-primary">حفظ البيانات</button>
+        </div>
+        <div class="col-12 text-end">
+            <button type="submit" class="btn btn-secondary">حفظ البيانات</button>
+        </div>
         </form>
     </div>
 @endsection
